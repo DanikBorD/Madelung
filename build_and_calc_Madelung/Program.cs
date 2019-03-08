75a87157ce9a49b25c92c56cc1a4be78d97a0870
Daniil Borisenko <danikbord@ya.ru>
2019-03-08 21:14:14 +0500

    add ion_params

--- build_and_calc_Madelung/Program.cs
+++ build_and_calc_Madelung/Program.cs
@@ -11,5 +11,11 @@ namespace build_and_calc_Madelung
         static void Main(string[] args)
         {
         }
+
+        private void build_crystal() // Функция по созданию кристаллической решетки
+        {
+            public float [][,,]  ion_params = new float[100][,,]; // массив для [заряд, X,Y,Z]
+            
+        }
     }
 }
