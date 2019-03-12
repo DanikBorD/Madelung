cb027ad90a5d9d98df532d1caba3bf3ffa8db3a4
Daniil Borisenko <danikbord@ya.ru>
2019-03-12 20:04:03 +0500

    need tests

--- build_and_calc_Madelung/Program.cs
+++ build_and_calc_Madelung/Program.cs
@@ -10,13 +10,41 @@ namespace build_and_calc_Madelung
     {
         static void Main(string[] args)
         {
-            int[,,] q = new int[100, 100, 100]; // массив с координатами и значением заряда в кажой точке кристалла
+            double[,,] q = new double[100, 100, 100]; // массив с координатами и значением заряда в кажой точке кристалла
             int dim = 10; // степень дробления одного кубика  
             int count = 10; // количество кубиков 
             string tmp = string.Empty;
             string charge = string.Empty;
+            string params_str = string.Empty;
+            double param_a = 0;
+            double param_b = 0;
+            double param_c = 0;
 
-            Console.WriteLine("Введите координаты X, Y, Z и заряд в данной точке (например, 7,53;3,67;5,41;8). Для окончания введите \"конец\"" + Environment.NewLine);
+            int coord_x = 0;
+            int coord_y = 0;
+            int coord_z = 0;
+            double coord_q = 0;
+
+            bool only_first = true;
+
+            int x_ = 0;
+            int y_ = 0;
+            int z_ = 0;
+            double q_ = 0;
+            double M = 0;
+
+            double r_min = 1000;
+            double r_tmp = 0;
+
+
+            Console.WriteLine("Введите параметры кристаллической решетки, например, 2.3;5.6;7.8" + Environment.NewLine);
+
+            params_str = Console.ReadLine();
+            param_a = Convert.ToDouble(params_str.Split(';')[0]);
+            param_b = Convert.ToDouble(params_str.Split(';')[1]);
+            param_c = Convert.ToDouble(params_str.Split(';')[2]);
+
+            Console.WriteLine("Введите координаты X, Y, Z и заряд в данной точке (например, 7.53;3.67;5.41;8). Для окончания введите \"конец\"" + Environment.NewLine);
 
             while (true)
             {
@@ -28,35 +56,63 @@ namespace build_and_calc_Madelung
                 //if(tmp.Split(';')[0].Contains('-'))
 
                 // foreach(var value in tmp.Split(';'))
-                q[(int)Math.Round(Convert.ToDouble(tmp.Split(';')[0])), 
-                    (int)Math.Round(Convert.ToDouble(tmp.Split(';')[1])), 
-                    (int)Math.Round(Convert.ToDouble(tmp.Split(';')[2]))] = Convert.ToInt16(tmp.Split(';')[3]); //проверить
+
+                coord_x = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[0]) * dim / param_a));
+                coord_y = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[1]) * dim / param_b));
+                coord_z = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[2]) * dim / param_c));
+                coord_q = Convert.ToDouble(tmp.Split(';')[3]);
+
+                if (only_first)
+                {
+                    x_ = coord_x;
+                    y_ = coord_y;
+                    z_ = coord_z;
+                    q_ = coord_q;
+                    only_first = false;
+                }
+
+                for (int x = 0; x < count; x++)
+                {
+                    for(int y = 0; y < count; y ++)
+                    {
+                        for(int z = 0; z < count; z ++)
+                        {
+                            int xx = coord_x + (x * dim);
+                            int yy = coord_y + (y * dim);
+                            int zz = coord_z + (z * dim);
+
+                            q[xx, yy, zz] = coord_q;
+
+                            if (coord_q * q_ < 0)
+                            {
+                                r_tmp = Math.Sqrt(((xx - x_) * (xx - x_) * param_a * param_a) + ((yy - y_) * (yy - y_) * param_b * param_b) +
+                                    ((zz - z_) * (zz - z_) * param_c * param_c)) / dim;
+
+                                if (r_min > r_tmp) r_min = r_tmp;
+                            }                            
+                        }
+                    }
+                }
 
                 Console.WriteLine();
 
             }
 
+            for (int i = 0; i < count * dim; i++)
+                for (int j = 0; j < count * dim; j++)
+                    for (int k = 0; k < count * dim; k++)
+                        if (!((i == x_) && (j == y_) && (k == z_)))
+                        {
+                            r_tmp = Math.Sqrt(((i - x_) * (i - x_) * param_a * param_a) + ((j - y_) * (j - y_) * param_b * param_b) +
+                                   ((k - z_) * (k - z_) * param_c * param_c)) / dim;
+
+                            M += q[i, j, k] * r_min / r_tmp;
+                        }
+
+            Console.WriteLine("Постоянная Маделунга = " + M.ToString());
+
             Console.ReadKey();
+
         }
-
-        // i – номер квадрата по длине, j – номер квадрата по ширине, k – номер квадрата по высоте
-
-        // x – координата в длину, y – координата в ширину, z – координата в высоту.
-
-        //        for (int i = 1; i < 10; i++) {
-        //            for (int j = 1; j < 10; j++) {
-        //                for (int k = 1; k < 10; k++) {
-        //                    for (int x = 0; x < 100; x++) {
-        //                        for (int y = 0; y < 100; y++) {
-        //                            for (int z = 0; z < 100; z++) {
-        //                                q[100 * i + x, 100 * j + y, 100 * k + z] = q[x,y,z];
-        //   
-        //}
-        //}
-        //}
-        //}
-        //}
-        //}
-
     }
 }
