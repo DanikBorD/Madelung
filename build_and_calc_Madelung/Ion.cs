3d8cf420ed8126565b3119968ddf81139047d05b
Daniil Borisenko <danikbord@ya.ru>
2019-03-13 20:37:20 +0500

    new version

---
+++ build_and_calc_Madelung/Ion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace build_and_calc_Madelung
+{
+    class Ion
+    {
+        public double x, y, z, q, r;
+
+        public Ion (string line)
+        {
+            x = Convert.ToDouble(line.Split(';')[0]);
+            y = Convert.ToDouble(line.Split(';')[1]);
+            z = Convert.ToDouble(line.Split(';')[2]);
+            q = Convert.ToDouble(line.Split(';')[3]);
+		}
+
+        public Ion (double a, double b, double c, double d)
+        {
+            x = a;
+            y = b;
+            z = c;
+            q = d;
+        }
+    }
+}
--- build_and_calc_Madelung/Program.cs
+++ build_and_calc_Madelung/Program.cs
@@ -10,7 +10,7 @@ namespace build_and_calc_Madelung
     {
         static void Main(string[] args)
         {
-            double[,,] q = new double[100, 100, 100]; // массив с координатами и значением заряда в кажой точке кристалла
+            //double[,,] q = new double[100, 100, 100]; // массив с координатами и значением заряда в кажой точке кристалла
             int dim = 10; // степень дробления одного кубика  
             int count = 10; // количество кубиков 
             string tmp = string.Empty;
@@ -27,14 +27,21 @@ namespace build_and_calc_Madelung
 
             bool only_first = true;
 
-            int x_ = 0;
-            int y_ = 0;
-            int z_ = 0;
+            double x_ = 0;
+            double y_ = 0;
+            double z_ = 0;
             double q_ = 0;
             double M = 0;
 
-            double r_min = 1000;
+            double r_min = 100000;
             double r_tmp = 0;
+
+            //-----------
+
+            List<Ion> ions = new List<Ion>();
+            int counter = 0; // количество ионов в одном "кубике" 
+            //int count_all = 0; // количество ионов во всей системе
+            List<double> r_tmp_list = new List<double>();
 
 
             Console.WriteLine("Введите параметры кристаллической решетки, например, 2.3;5.6;7.8" + Environment.NewLine);
@@ -53,66 +60,90 @@ namespace build_and_calc_Madelung
 
                 if (tmp == "конец") break;
 
-                //if(tmp.Split(';')[0].Contains('-'))
+                ions.Add(new Ion(tmp));
 
-                // foreach(var value in tmp.Split(';'))
-
-                coord_x = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[0]) * dim / param_a));
-                coord_y = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[1]) * dim / param_b));
-                coord_z = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[2]) * dim / param_c));
-                coord_q = Convert.ToDouble(tmp.Split(';')[3]);
-
-                if (only_first)
-                {
-                    x_ = coord_x;
-                    y_ = coord_y;
-                    z_ = coord_z;
-                    q_ = coord_q;
-                    only_first = false;
-                }
-
-                for (int x = 0; x < count; x++)
-                {
-                    for(int y = 0; y < count; y ++)
-                    {
-                        for(int z = 0; z < count; z ++)
-                        {
-                            int xx = coord_x + (x * dim);
-                            int yy = coord_y + (y * dim);
-                            int zz = coord_z + (z * dim);
-
-                            q[xx, yy, zz] = coord_q;
-
-                            if (coord_q * q_ < 0)
-                            {
-                                r_tmp = Math.Sqrt(((xx - x_) * (xx - x_) * param_a * param_a) + ((yy - y_) * (yy - y_) * param_b * param_b) +
-                                    ((zz - z_) * (zz - z_) * param_c * param_c)) / dim;
-
-                                if (r_min > r_tmp) r_min = r_tmp;
-                            }                            
-                        }
-                    }
-                }
-
-                Console.WriteLine();
-
+                counter++;
             }
 
-            for (int i = 0; i < count * dim; i++)
-                for (int j = 0; j < count * dim; j++)
-                    for (int k = 0; k < count * dim; k++)
-                        if (!((i == x_) && (j == y_) && (k == z_)))
+            //if(tmp.Split(';')[0].Contains('-'))
+
+            // foreach(var value in tmp.Split(';'))
+
+            //coord_x = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[0]) * dim / param_a));
+            //coord_y = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[1]) * dim / param_b));
+            //coord_z = Convert.ToInt16(Math.Round(Convert.ToDouble(tmp.Split(';')[2]) * dim / param_c));
+            //coord_q = Convert.ToDouble(tmp.Split(';')[3]);
+
+            //if (only_first)
+            //{
+            //    x_ = coord_x;
+            //    y_ = coord_y;
+            //    z_ = coord_z;
+            //    q_ = coord_q;
+            //    only_first = false;
+            //}
+
+            for (int h = 0; h < count; h++)
+            {
+                for(int al = 0; al < count; al ++)
+                {
+                    for(int l = 0; l < count; l ++)
+                    {
+                        if (h == 0 && al == 0 && l == 0) continue;
+                        for (int k = 0; k < counter; k++)
                         {
-                            r_tmp = Math.Sqrt(((i - x_) * (i - x_) * param_a * param_a) + ((j - y_) * (j - y_) * param_b * param_b) +
-                                   ((k - z_) * (k - z_) * param_c * param_c)) / dim;
+                            double x = ions[k].x + param_a * h;
+                            double y = ions[k].y + param_b * al;
+                            double z = ions[k].z + param_c * l;
+                            double q = ions[k].q;
+                            ions.Add(new Ion(x, y, z, q));
+                        }                           
+                    }
+                }
+            }
 
-                            M += q[i, j, k] * r_min / r_tmp;
-                        }
+            x_ = ions.First().x;
+            y_ = ions.First().y;
+            z_ = ions.First().z;
+            q_ = ions.First().q;
 
+            foreach(var ion in ions)
+            {
+                if (ion == ions.First()) continue;
+
+                double x = ion.x;
+                double y = ion.y;
+                double z = ion.z;
+                double q = ion.q;
+
+                ion.r = r_tmpCalc(x_, y_, z_, x, y, z);
+                //r_tmp_list.Add(r_tmp);
+
+                if (q_ * q < 0 && r_min > ion.r)
+                    r_min = ion.r;
+            }
+
+            foreach(var ion in ions)
+            {
+                if (ion == ions.First()) continue;
+
+                M += ion.q * r_min / ion.r;
+            }
+
+            foreach (var item in ions.Take(20))
+                Console.WriteLine("x = " + item.x.ToString() + ", y = " + item.y.ToString() + ", z = " + 
+                    item.z.ToString() + ", q = " + item.q.ToString());
+
+            Console.WriteLine("r_min = " + r_min.ToString());
             Console.WriteLine("Постоянная Маделунга = " + M.ToString());
 
             Console.ReadKey();
 
+        }
+
+        public static double r_tmpCalc(double x1, double y1, double z1, double x2, double y2, double z2)
+        {            
+            return Math.Sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) + ((z1 - z2) * (z1 - z2)));
         }
     }
 }
